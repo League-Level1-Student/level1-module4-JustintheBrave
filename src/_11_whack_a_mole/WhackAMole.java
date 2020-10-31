4938a57357778c656eeb6d3b82eeabb01b99e8d0
JustintheBrave <55635392+JustintheBrave@users.noreply.github.com>
2020-10-30 17:58:44 -0700

    whack

--- src/_11_whack_a_mole/WhackAMole.java
+++ src/_11_whack_a_mole/WhackAMole.java
@@ -1,10 +1,16 @@
 package _11_whack_a_mole;
 
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class WhackAMole {
+public class WhackAMole implements ActionListener {
 	
 	public static void main(String args[]) {
 		WhackAMole mole = new WhackAMole();
@@ -15,12 +21,33 @@ public class WhackAMole {
 	static JPanel panel = new JPanel();
 	
 	public void GUI() {
-		for (int i = 0; i < 24; i++) {
-			JButton button = new JButton();
-			panel.add(button);
-		}
+		System.out.println("test");
+		frame.setTitle("Whack a Mole");
 		frame.add(panel);
 		frame.setVisible(true);
-		frame.pack();
+		frame.setPreferredSize(new Dimension (250, 300));
+		drawButtons();
+	}
+	
+	
+	public void drawButtons() {
+		Random ran = new Random();
+		int moleNum = ran.nextInt(24);
+		
+		for (int i = 0; i < 24; i++) {
+			JButton button = new JButton();
+			if(i==moleNum) {
+				button.setText("Mole!");
+			}
+			panel.add(button);
+			button.addActionListener(this);
+		}
+	}
+
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 }
