dcfb9648ac090b8359983653eb14e6021cd3294b
JustintheBrave <55635392+JustintheBrave@users.noreply.github.com>
2020-10-30 18:28:06 -0700

    whack a mole
    :sunglasses:

--- src/_11_whack_a_mole/WhackAMole.java
+++ src/_11_whack_a_mole/WhackAMole.java
@@ -3,6 +3,7 @@ package _11_whack_a_mole;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Date;
 import java.util.Random;
 
 import javax.swing.JButton;
@@ -19,13 +20,14 @@ public class WhackAMole implements ActionListener {
 	
 	static JFrame frame = new JFrame();
 	static JPanel panel = new JPanel();
+	static int moleHits = 0;
 	
 	public void GUI() {
-		System.out.println("test");
 		frame.setTitle("Whack a Mole");
 		frame.add(panel);
 		frame.setVisible(true);
 		frame.setPreferredSize(new Dimension (250, 300));
+		frame.pack();
 		drawButtons();
 	}
 	
@@ -43,11 +45,48 @@ public class WhackAMole implements ActionListener {
 			button.addActionListener(this);
 		}
 	}
+	
+    static void speak(String words) {
+        if( System.getProperty( "os.name" ).contains( "Windows" ) ) {
+            String cmd = "PowerShell -Command \"Add-Type -AssemblyName System.Speech; (New-Object System.Speech.Synthesis.SpeechSynthesizer).Speak('"
+                    + words + "');\"";
+            try {
+                Runtime.getRuntime().exec( cmd ).waitFor();
+            } catch( Exception e ) {
+                e.printStackTrace();
+            }
+        } else {
+            try {
+                Runtime.getRuntime().exec( "say " + words ).waitFor();
+            } catch( Exception e ) {
+                e.printStackTrace();
+            }
+        }
+    }
+    
+    private void endGame(Date timeAtStart, int molesWhacked) { 
+        Date timeAtEnd = new Date();
+        JOptionPane.showMessageDialog(null, "Your whack rate is "
+                + ((timeAtEnd.getTime() - timeAtStart.getTime()) / 1000.00 / molesWhacked)
+                      + " moles per second.");
+    }
 
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
+		JButton buttonPressed = (JButton) e.getSource();
+		if(! buttonPressed.getText().equalsIgnoreCase("Mole!")) {
+			speak("ur bad");	
+	}
+		if(buttonPressed.getText().equalsIgnoreCase("Mole!")) {
+			moleHits++;	
+	}
+		if(moleHits>=10) {
+
+		}
+		frame.dispose();
+		drawButtons();
 		
 	}
 }
