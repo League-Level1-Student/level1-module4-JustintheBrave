ee9c1f067a8cbce2c9609d11aea3b35e76b7725f
JustintheBrave <55635392+JustintheBrave@users.noreply.github.com>
2020-11-04 11:09:04 -0800

    slot

--- src/_12_slot_machine/SlotMachine.java
+++ src/_12_slot_machine/SlotMachine.java
@@ -1,7 +1,13 @@
 package _12_slot_machine;
 
 import java.awt.Dimension;
+import java.net.MalformedURLException;
+import java.util.Random;
 
+import javax.print.DocFlavor.URL;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -13,11 +19,12 @@ public class SlotMachine {
 	static JLabel label1 = new JLabel();
 	static JLabel label2 = new JLabel();
 	static JLabel label3 = new JLabel();
+	static JButton button = new JButton();
 	
 	public void GUI() {
 		frame.setVisible(true);
 		frame.setTitle("Slot Machine");
-		frame.setPreferredSize(new Dimension (450, 150));
+		frame.setPreferredSize(new Dimension (450, 200));
 		panel.add(label1);
 		panel.add(label2);
 		panel.add(label3);
@@ -25,6 +32,10 @@ public class SlotMachine {
 		frame.pack();
 	}
 	
+	public void slots() {
+		Random ran = new Random();
+		int chance = ran.nextInt(2);
+	}
 	
-	
+
 }
